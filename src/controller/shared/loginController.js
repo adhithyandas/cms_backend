c49918381df4c6fa3291b18af2cf62290a8257d5
adhithyandas <adhithyandask@gmail.com>
2026-06-12 11:56:12 +0530

    fix: enable cross-origin requests and update cookie security settings to allow cross-site access

--- src/controller/shared/loginController.js
+++ src/controller/shared/loginController.js
@@ -22,8 +22,8 @@ const loginAdmin = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000
         });
 
